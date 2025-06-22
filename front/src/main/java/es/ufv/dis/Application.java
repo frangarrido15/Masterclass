b5738ff94f3bac9a7496e75c110c928ca43663f0
Francisco Javier Garrido Peso <fran.garrido.peso@gmail.com>
2025-06-22 18:41:48 +0200

    front finiquitao

--- front/src/main/java/es/ufv/dis/Application.java
+++ front/src/main/java/es/ufv/dis/Application.java
@@ -1,13 +1,15 @@
 package es.ufv.dis;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@Theme("default")
+@PWA(name = "Masterclass Vaadin", shortName = "Masterclass Vaadin",
+        description = "Aplicación de ejemplo con Vaadin y Spring Boot")
 public class Application implements AppShellConfigurator {
 
 
